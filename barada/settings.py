0213e1c374aea95262bc648e59851d804d2e466d
Dancha <Dancha241@gmail.com>
2019-10-25 16:06:01 +0700

    Нстройка базы данных и первая модел игр

--- barada/settings.py
+++ barada/settings.py
@@ -77,8 +77,11 @@ WSGI_APPLICATION = 'barada.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'barada',
+        'USER': 'barada',
+        'PASSWORD': 'Barada228',
+        'HOST': '194.67.86.144',
     }
 }
 
---
+++ games/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Games(models.Model):
+    name = models.CharField(max_length=200, verbose_name='Название')
+    nameoriginal = models.CharField(max_length=200, verbose_name='Оригинальное название')
+    have = models.BooleanField(verbose_name='Наличие игры')
+    min_player = models.ImageField()
+    max_player = models.ImageField()
+    disc = models.TextField(verbose_name='Описание')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Игра'
+        verbose_name_plural = 'Игры'
+
